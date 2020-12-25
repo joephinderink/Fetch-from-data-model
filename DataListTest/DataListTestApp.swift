46848bc6ffc11afacd4d5df2cddf2e18143906cf
Joep Hinderink <hello@joephinderink.nl>
2020-12-25 17:50:28 +0100

    Initial Commit

---
+++ DataListTest/DataListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataListTestApp.swift
+//  DataListTest
+//
+//  Created by Joep Hinderink on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct DataListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
